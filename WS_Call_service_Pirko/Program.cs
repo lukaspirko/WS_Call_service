5d125df1cc6547163217a7dbed46950db57a58e7
Lukáš Pírko <pirko.lucas@gmail.com>
2020-05-10 00:29:20 +0200

    Přidejte soubory projektu.

---
+++ WS_Call_service_Pirko/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace WS_Call_service_Pirko
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Call WS:\n");
+            CallWS();
+            Console.WriteLine("WS called");
+            Console.ReadKey();
+
+        }
+
+        static void CallWS()
+        {
+            ServiceReference1.GeneratedNg_predmetyServiceClient client = new ServiceReference1.GeneratedNg_predmetyServiceClient();
+            var resp = client.searchSimplePredmetyAsync(new ServiceReference1.searchSimplePredmety("", "", "", "", "IC++1", "", "", "", "", "", "", "", "", "", ""));
+
+            foreach (var subject in resp.Result.ws_stag_predmety_SearchSimplePredmety)
+            {
+                Console.WriteLine("Finded: " + subject.nazev + " " + subject.rokVarianty);
+            }
+        }
+    }
+}
